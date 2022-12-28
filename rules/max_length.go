fd006eb68539c1a80271d5760d3e8635101a8970
Behzad Shabani <behzad.shabani@gmail.com>
2022-12-28 23:57:39 +0330

    Add Length related rules

---
+++ rules/max_length.go
@@ -0,0 +1,54 @@
+package rules
+
+import (
+	"reflect"
+
+	"github.com/spf13/cast"
+
+	"github.com/behzadsh/go.validator/bag"
+	"github.com/behzadsh/go.validator/translation"
+)
+
+// MaxLength checks the field under validation length did not reach given length.
+//
+// Usage: "maxLength:value"
+// Example: "maxLength:10"
+type MaxLength struct {
+	translation.BaseTranslatableRule
+	maxLength int
+}
+
+// Validate does the validation process of the rule. See struct documentation
+// for more details.
+func (r *MaxLength) Validate(selector string, value any, inputBag bag.InputBag) Result {
+	kind := reflect.TypeOf(value).Kind()
+
+	var length int
+	switch kind {
+	case reflect.String, reflect.Slice, reflect.Array, reflect.Map:
+		length = reflect.ValueOf(value).Len()
+	default:
+		// ignore the rule if not match any of the specified types.
+		return NewSuccessResult()
+	}
+
+	if length > r.maxLength {
+		return NewFailedResult(r.Translate(r.Locale, "validation.max_length", map[string]string{
+			"field": selector,
+			"value": cast.ToString(r.maxLength),
+		}))
+	}
+
+	return NewSuccessResult()
+}
+
+// AddParams adds rules parameter values to the rule instance.
+func (r *MaxLength) AddParams(params []string) {
+	r.maxLength = cast.ToInt(params[0])
+}
+
+// MinRequiredParams returns minimum parameter requirement for this rule.
+// This rule need only one parameter that is the `length`.
+func (r *MaxLength) MinRequiredParams() int {
+	return 1
+}
---
+++ rules/max_length_test.go
@@ -0,0 +1,155 @@
+package rules
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/behzadsh/go.validator/bag"
+)
+
+var maxLengthRuleTestData = map[string]any{
+	"successfulString": map[string]any{
+		"input": map[string]any{
+			"selector": "functionName",
+			"inputBag": bag.InputBag{
+				"functionName": "doSomething",
+			},
+			"params": []string{
+				"15",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": false,
+			"validationError":  "",
+		},
+	},
+	"successfulSlice": map[string]any{
+		"input": map[string]any{
+			"selector": "skills",
+			"inputBag": bag.InputBag{
+				"skills": []string{"Go", "Software Engineering", "TDD", "Software Architecture"},
+			},
+			"params": []string{
+				"5",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": false,
+			"validationError":  "",
+		},
+	},
+	"successfulMap": map[string]any{
+		"input": map[string]any{
+			"selector": "user",
+			"inputBag": bag.InputBag{
+				"user": map[string]any{
+					"userName": "johnDoe",
+					"name":     "John Doe",
+					"age":      35,
+				},
+			},
+			"params": []string{
+				"3",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": false,
+			"validationError":  "",
+		},
+	},
+	"failedString": map[string]any{
+		"input": map[string]any{
+			"selector": "functionName",
+			"inputBag": bag.InputBag{
+				"functionName": "doSomethingUseful",
+			},
+			"params": []string{
+				"10",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": true,
+			"validationError":  "The field functionName must not have a length more than 10.",
+		},
+	},
+	"failedSlice": map[string]any{
+		"input": map[string]any{
+			"selector": "skills",
+			"inputBag": bag.InputBag{
+				"skills": []string{"Go", "Software Engineering", "TDD", "Software Architecture"},
+			},
+			"params": []string{
+				"3",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": true,
+			"validationError":  "The field skills must not have a length more than 3.",
+		},
+	},
+	"failedMap": map[string]any{
+		"input": map[string]any{
+			"selector": "user",
+			"inputBag": bag.InputBag{
+				"user": map[string]any{
+					"userName": "johnDoe",
+					"name":     "John Doe",
+					"age":      35,
+				},
+			},
+			"params": []string{
+				"2",
+			},
+		},
+		"output": map[string]any{
+			"validationFailed": true,
+			"validationError":  "The field user must not have a length more than 2.",
+		},
+	},
+}
+
+func TestMaxLengthRule(t *testing.T) {
+	rule := initMaxLengthRule()
+
+	for name, d := range maxLengthRuleTestData {
+		t.Run(name, func(t *testing.T) {
+			data := d.(map[string]any)
+			input := data["input"].(map[string]any)
+			output := data["output"].(map[string]any)
+			inputBag := input["inputBag"].(bag.InputBag)
+
+			value, _ := inputBag.Get(input["selector"].(string))
+
+			rule.AddParams(input["params"].([]string))
+			res := rule.Validate(input["selector"].(string), value, inputBag)
+
+			assert.Equal(t, output["validationFailed"].(bool), res.Failed())
+			assert.Equal(t, output["validationError"].(string), res.Message())
+		})
+	}
+}
+
+func initMaxLengthRule() *MaxLength {
+	maxLengthRule := &MaxLength{}
+	maxLengthRule.AddTranslationFunction(func(_, key string, params ...map[string]string) string {
+		var p map[string]string
+		if len(params) > 0 {
+			p = params[0]
+		}
+
+		switch key {
+		case "validation.max_length":
+			tr := "The field :field: must not have a length more than :value:."
+			for k, v := range p {
+				tr = strings.Replace(tr, ":"+k+":", v, -1)
+			}
+
+			return tr
+		default:
+			return key
+		}
+	})
+	return maxLengthRule
+}
